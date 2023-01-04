d2851a2b49cde86c004d65c3fd5124df86e6372e
Alex Chong <alex.chong@psg-solutions.com>
2023-01-04 10:32:12 -0800

    fix refresh token on startup

--- src/app/actions/startupAsync.ts
+++ src/app/actions/startupAsync.ts
@@ -1,6 +1,7 @@
 import { DecodedToken } from "@pci/pci-services.types.decoded-token";
 
 import { AppDispatch, RootState } from "~/app/store";
+import { refreshAccessToken } from "~/features/auth/actions/refreshAccessToken";
 import { getAccessToken } from "~/features/auth/authSlice";
 import { setUser } from "~/features/auth/userSlice";
 import { queryApi } from "~/services/queryApi";
@@ -11,6 +12,8 @@ export const startupAsync =
     const accessToken = getAccessToken(getState());
 
     if (accessToken) {
+      // force a refresh of access token;
+      await dispatch(refreshAccessToken(true));
       const me = await dispatch(queryApi.endpoints.getMe.initiate());
       console.log("user loaded", me.data?.username);
       dispatch(setUser(me.data as DecodedToken));

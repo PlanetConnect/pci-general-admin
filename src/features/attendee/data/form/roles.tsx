07a809e77813d830cb035c3cf86a33dd59d7c764
Alex Chong <alex.chong@psg-solutions.com>
2023-01-25 12:27:02 -0800

    update roles to text

--- src/features/attendee/data/form/roles.tsx
+++ src/features/attendee/data/form/roles.tsx
@@ -1,26 +1,26 @@
 const roles = [
   {
-    value: "43da751a-50db-43ad-b74d-818cbb77b37e",
+    value: "Pharma Attendee",
     label: "Pharma Attendee",
   },
   {
-    value: "81107b52-e46b-4c31-8e6a-4c1e2e46f896",
+    value: "Reporting",
     label: "Reporting",
   },
   {
-    value: "496c6b4a-bbe3-464c-8782-d9ff8f53aa1a",
+    value: "Booth Staff",
     label: "Booth Staff",
   },
   {
-    value: "3440c346-11cf-4da7-9cc1-bd2586e1d02e",
+    value: "Event Administrator",
     label: "Event Administrator",
   },
   {
-    value: "673f9a67-1ded-4a9e-ba7b-b6d96641bf08",
+    value: "Test Role",
     label: "Test Role",
   },
   {
-    value: "37668e22-15ad-44e0-9de6-165f75a6b15e",
+    value: "Test Role 2",
     label: "Test Role 2",
   },
 ];
